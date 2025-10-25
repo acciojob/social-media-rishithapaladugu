6c713660e38ec3f4eb42f5272a942df99e938775
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-10-25 12:52:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,32 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./Navbar";
-import Home from "../pages/Home";
-import Users from "../pages/Users";
-import NotificationsPage from "../pages/NotificationsPage";
-import PostDetail from "../pages/PostDetail";
 
+
+import React, { useState } from 'react';
+import Tabs from './Tabs';
+import PostForm from './PostForm';
+import PostList from './PostList';
+import Users from './Users';
+import Notifications from './Notifications';
 export default function App() {
+  const [activeTab, setActiveTab] = useState('posts');
+
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/notifications" element={<NotificationsPage />} />
-          <Route path="/posts/:id" element={<PostDetail />} />
-        </Routes>
-      </Router>
+    <div className="App"> {/* <-- Add this class */}
+      <h1>GenZ</h1>
+      <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
+      {activeTab === 'posts' && (
+        <>
+          <PostForm />
+          <h2>Posts</h2>
+          <PostList />
+        </>
+      )}
+       <div className="tabs">
+      <a href="#" onClick={() => setActiveTab('posts')}>Posts</a>
+      <a href="#" onClick={() => setActiveTab('users')}>Users</a>
+      <a href="#" onClick={() => setActiveTab('notifications')}>Notifications</a>
+    </div>
+      {activeTab === 'users' && <Users />}
+      {activeTab === 'notifications' && <Notifications />}
     </div>
   );
 }
