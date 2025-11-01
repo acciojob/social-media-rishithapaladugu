aac776cbc4157a5a1f767f38442d7386793efad2
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-11-01 22:21:11 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,16 @@
-import React from "react";
-import { Link } from "react-router-dom";
+// src/components/Navbar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function Navbar() {
+function Navbar() {
   return (
     <nav>
-      <Link to="/">Posts</Link> |<Link to="/users">Users</Link> |
+      <Link to="/">Posts</Link>
+      <Link to="/users">Users</Link>
       <Link to="/notifications">Notifications</Link>
+      <Link to="/create-post">Create Post</Link>
     </nav>
   );
 }
+
+export default Navbar;
